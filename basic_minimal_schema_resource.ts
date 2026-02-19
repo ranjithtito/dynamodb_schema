1a3add7b429081ad97e2add53643c45d2e1d40cf
ranjithtito <ranjith@tito.org.in>
2026-02-19 16:05:17 +0530

    Add basic minimal schema resource definition

---
+++ basic_minimal_schema_resource.ts
@@ -0,0 +1,19 @@
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+const schema = a.schema({
+  Test: a.model({
+    content: a.string(),
+  })
+  .authorization((allow) => [allow.publicApiKey()]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "apiKey",
+    apiKeyAuthorizationMode: {
+      expiresInDays: 30,
+    },
+  },
+});
